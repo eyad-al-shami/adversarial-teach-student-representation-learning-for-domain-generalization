9697b9f076c58784876b04a43c27f4e9eabf0926
eyad-al-shami <eyad.m.alshami@gmail.com>
2023-03-04 21:49:55 +0100

    fix discrepancy loss in augmenter training

--- train.py
+++ train.py
@@ -240,17 +240,22 @@ def ABatchTraining(augmenter, teacher, student, classifier, batch):
     # compute the output of the augmenter
     augmented_data = augmenter(batch[0])
     # compute the output of the student on the augmented data
-    student_output = student(augmented_data)
+    s_output = student(augmented_data)
     # compute the output of the teacher on the original data
-    teacher_output = teacher(batch[0])
-    # compute the discrepancy between the teacher and the student
-    discrepancy = discripancy_loss(teacher_output - student_output, dim=1)
+    t_output = teacher(batch[0])
+    with torch.no_grad():
+        t_output_magnitude = torch.linalg.vector_norm(t_output, dim=1, keepdim=True)
+        s_output_magnitude = torch.linalg.vector_norm(s_output, dim=1, keepdim=True)
+    t_output_normalized = t_output / t_output_magnitude
+    s_output_normalized = s_output / s_output_magnitude
+    discrepancy = t_output_normalized - s_output_normalized
+    discrepancy_loss = torch.einsum('ij, ij -> i', discrepancy, discrepancy).mean()
     # compute the minimum between 0 and the margin - discrepancy
-    margin_loss = torch.min(discrepancy - margin, torch.tensor(0))
+    margin_loss = torch.min(discrepancy_loss - margin, torch.tensor(0))
     # TODO: check if this is correct
     margin_loss = margin_loss.sum()
     # compute the cross entropy loss between the student output and the target
-    cross_entropy = cross_entropy_loss(classifier(student_output), batch[2])
+    cross_entropy = cross_entropy_loss(classifier(s_output), batch[2])
     # compute the total loss and update the augmenter
     loss = -margin_loss + cross_entropy
     loss.backward()
