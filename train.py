a6af6e1043af01e6fc4087f2305249aa96fb08d5
eyad-al-shami <eyad.m.alshami@gmail.com>
2023-04-03 14:21:08 +0200

    update teacher every 3 epochs

--- train.py
+++ train.py
@@ -86,7 +86,7 @@ def training_validation_loop(cfg, logger):
 
                 rl_loss = teacher_studnet_batch_training(augmenter, teacher, student, classifier, student_optimizer, batch, epoch)
                 # if (cfg.MODEL.TEACHER.UPDATE_TEACHER and epoch >= cfg.MODEL.TEACHER.WARMUP_EPOCHS):
-                if (epoch % 2 == 0):
+                if (epoch % 3 == 0):
                     teacher = update_teacher(teacher, student, cfg.MODEL.TEACHER.TAU)
                 aug_D_loss, aug_Ce_Loss = augmenter_batch_training(augmenter, teacher, student, classifier, augmenter_optimizer, batch)
 
